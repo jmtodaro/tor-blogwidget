1c9a5b851ae3552bb593dab9ad2b6be0eb9dbde8
Jeremy M. Todaro <jmtodaro@gmail.com>
2014-09-20 14:53:43 -0500

    converted to use rss but now requires XML::RSS

--- construct.pl
+++ construct.pl
@@ -1,89 +1,114 @@
 #!/usr/bin/perl
+  use strict;
+  use warnings;
   use open ':std', ':encoding(UTF-8)';
-  
-  my $url = 'https://blog.torproject.org';
+  use LWP::Simple;
+  use XML::RSS;
+
+  # RSS feed url
+  my $url = 'https://blog.torproject.org/blog/feed';
   # Number of posts to show
   my $showPosts = "2";
-  # Maximum title characters to allow before truncating
-  my $maxLength = "35";
+  # Maximum characters in post title to allow before truncating
+  my $titleMaxLength = "35";
+  # Maximum characters in author name to allow before truncating
+  my $authorMaxLength = "15";
+  
+  # Retreive url
+  my $data = get( $url );
+  
+  if ($data) # Url returned data
+  {
+    my $check = $data;
+    $check =~ m{<rss(.*?)>}i;
+    my $checkVal = $1;
 
-  use LWP::Simple;
-  my $content = get $url;
+    if ($checkVal) # Is an rss feed
+    {
+      my $rss = XML::RSS->new();
+      $rss->parse( $data );
+        
+      #my $channel = $rss->{channel};
+      #my $title   = $channel->{title};
+      #my $link    = $channel->{link};
+      #my $desc    = $channel->{description};
+      my @items;
+      my $item;
+    
+      print "<div class='blogFeed'>
+             <div class='blogFirstRow'>
+             <h2>Recent Blog Posts</h2>
+             </div>";
+         
+      # Store rss post data in array
+      foreach $item ( @{ $rss->{items} } )
+      {
+        push(@items, $item);
+      }
 
-  # Check if we have the right data
-  my $check = "check";
-  if ($content) {
-    $check = $content;
-  }
-  $check =~ m{Tor\s*-\s*Home(.*?)</a>}i;
-  my $checkPage = $1;
+      # Generate posts
+      $showPosts = $showPosts - 1;           
+      for my $i (0..$showPosts)
+      {  
+        my $item = $items[$i];
+      	my $link  = $item->{link};
+      	my $title = $item->{title};
+      	my $date = $item->{pubDate};
+      	my $author = $item->{dc}->{creator};
+      	#my $desc = $item->{description};
+      	
+      	# Check title length and trim if necessary
+      	my $titleLength = length($title);
+      	my $titleTrim = $title;
+        if ($titleLength > $titleMaxLength)
+      	{
+    	    my $trimLength = $titleMaxLength - 3;
+    	    my $titleTxt = substr($title, 0, $trimLength);
+    	    $titleTrim = "$titleTxt...";
+      	}
+    
+      	# Trim date
+      	my $dateTrim = substr($date, 0, -15);
+      	
+      	# Check author length and trim if necessary
+      	my $authorLength = length($author);
+      	my $authorTrim = $author;
+        if ($authorLength > $authorMaxLength)
+      	{
+    	    my $authorTrimLength = $authorMaxLength - 3;
+    	    my $authorTxt = substr($author, 0, $authorTrimLength);
+    	    $authorTrim = "$authorTxt...";
+      	}
 
-  my $titleStr = $content;
-  my $linkStr = $content;
-  my $dateStr = $content;
-  my $authorStr = $content;
-  $showPosts = $showPosts - 1;
-
-  print "<div class='blogFeed'>
-         <div class='blogFirstRow'>
-         <h2>Recent Blog Posts</h2>
-         </div>";
-             
-  if ($checkPage) {  # Page check passed
-      for my $i (0..$showPosts) { 
-	
-	# Parse title
-	$titleStr =~ m{>(.*?)</a>\s*</h2>}g;
-	my $titleVal = $1;
-	my $titleLength = length($titleVal);
-	my $title = "";
-	
-	# Check title length and trim if necessary
-	if ($titleLength > $maxLength) {
-	    my $trimLength = $maxLength - 3;
-	    my $titleTxt = substr($titleVal, 0, $trimLength);
-	    $title = "$titleTxt...";
-	} else {
-	    $title = $titleVal;
-	}
-
-	# Parse link
-	$linkStr =~ m{<h2 class="title">\s*<a href="(.*?)">}g;
-	my $linkSuf = $1;
-	my $link = "$url$linkSuf";
-	
-	# Parse date
-	$dateStr =~ m{>(.*?)</abbr>}g;
-	my $date = $1;
-
-	# Parse author
-	$authorStr =~ m{</abbr>\s*by\s*(.*?)\s*</span>}g;
-	my $author = $1;
-	
-	# Begin html output
-	print "<a href=\'$link\' title=\'$titleVal\'>";
-	
-	# Required for alternating row colors
-	if (0 == $i % 2) {
-	    print "<div class='blogRow blogRow0'>";
-	} else {
-	    print "<div class='blogRow blogRow1'>";
-	}
-	
-	print "<p class='blogTitle'>$title</p>
-	      <p class='blogDate'>$date</p>
-	      <p class='blogAuthor'><a href=\"$url/blogs/$author\">Posted by: <em>$author</em></a></p>
-	      </div>
-	      </a>";    
+      	# Begin html output
+      	print "<a href=\'$link\' title=\'$title\'>";
+      	
+      	# Required for alternating row colors - switch blogRow# to change order
+      	if (0 == $i % 2) {
+      	    print "<div class='blogRow blogRow0'>";
+      	} else {
+      	    print "<div class='blogRow blogRow1'>";
+      	}
+      	
+      	print "<p class='blogTitle'>$titleTrim</p>
+      	      <p class='blogDate'>$dateTrim</p>
+      	      <p class='blogAuthor'>Posted by: <em>$author</em></p>
+      	      </div>
+      	      </a>";    
       }
       
-  } else {	# Page check failed - display message
+    } else {	# Not an rss feed
       print "<div class='blogRow blogRow1'>";
       print "<br /><p class='blogDate' style=\"text-align:center;color:\#999;line-height:16px;\"><em>Recent posts are temporarily unavailable</em></p></div>";    
+    }
+  
+  } else {	# Url did not return any data
+    print "<div class='blogRow blogRow1'>";
+    print "<br /><p class='blogDate' style=\"text-align:center;color:\#999;line-height:16px;\"><em>Recent posts are temporarily unavailable</em></p></div>";    
   }
   
   print "<a href='https://blog.torproject.org' title='Tor Blog Home'>
-	<div class='blogRow blogLastRow'>
+  <div class='blogRow blogLastRow'>
 	<p>View all blog posts &raquo;</p>
 	</div>
 	</a>
